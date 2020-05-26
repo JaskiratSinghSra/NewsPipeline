a5faa33af599faee3e540f07251bcf0a476f84a8
Jaskirat Singh <srajaskirat@gmail.com>
2020-05-26 01:32:21 -0400

    Added Logging

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from time import sleep
 import os
 import sys
 
-API_KEY = ''
+API_KEY = '8e762f6fc7634b1a8268c27ea76a3a7f'
 from datetime import datetime, timedelta, timezone
+FORMAT = '[%(asctime)s] [%(levelname)s] [%(name)s.%(funcName)s %(filename)s:%(lineno)d] %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 
 class Pipeline:
@@ -20,14 +22,14 @@ class Pipeline:
         end_date = datetime.now()
         start_date = start_date.replace(tzinfo=timezone.utc).timestamp()
         end_date = end_date.replace(tzinfo=timezone.utc).timestamp()
-        print("Fetching articles from News API")
+        logging.info("Fetching articles from News API")
         articles = self.news_client.get_everything(q='Google',
                                                    from_param=start_date,
                                                    to=end_date,
                                                    language='en',
                                                    sort_by='relevancy',
                                                    page=2)
-        print("Total articles %s fetched in this run." % (articles['totalResults']))
+        logging.info("Total articles %s fetched in this run." % (articles['totalResults']))
         return articles
 
     def run(self):
